6e4f1e53ae59f493cf826c6380ca16d42cd8855d
Samriddha Jung Pande <Samriddhajungpande@gmail.com>
2026-06-19 09:26:02 +0545

    Changed html structure using JS (with objects), made the quantity popup in top right interactive and each Add to cart button interactive (displays in console for now)

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,24 +1,7 @@
-const products=[{
-  image:"images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating:{stars:4.5,count:87},
-  priceCents: 1090
-}
-,{
-  image:"images/products/intermediate-composite-basketball.jpg",
-  name:" Intermediate Size Basketball",
-  rating:{stars:4,count:127},
-  priceCents:"2095"
-},
-{
-  image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name:" Adults Plain Cotton T-Shirt - 2 Pack",
-  rating:{stars:4.5,count:56},
-  priceCents:"799"
-}];
 
+let productsHTML=''
 products.forEach((product)=>{
-  const html = `
+  let html = `
     <div class="product-container">
       <div class="product-image-container">
         <img class="product-image"
@@ -31,17 +14,17 @@ products.forEach((product)=>{
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.ratings.stars*10}.png">
+          src="images/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
     </div>
     <div class="product-price">
-            ${product.priceCents/100}
+            ${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -62,11 +45,46 @@ products.forEach((product)=>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
-  
+
   `;
-  console.log(html);
+  productsHTML+=html;
+    
 })
+
+document.querySelector('.js-products-grid')
+  .innerHTML=productsHTML;
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button)=>{
+    button.addEventListener('click',()=>{
+      const productId=button.dataset.productId;
+      let StrQuan=document.querySelector(`.js-quantity-selector-${productId}`)
+        .value
+      let matchingitem;
+      cart.forEach((item)=>{
+        if (productId===item.productId){
+          matchingitem=item;
+        }
+      })
+       if(matchingitem){
+          matchingitem.Quantity+=Number(StrQuan);
+        }
+        else{
+          cart.push({
+          productId: productId,
+          Quantity: Number(StrQuan)
+          })
+        }
+        let cartQuantity=0;
+        cart.forEach((item)=>{
+          cartQuantity+=item.Quantity
+        })
+        document.querySelector('.js-cart-quantity')
+          .innerHTML=cartQuantity
+    console.log(cart);
+    })
+  })
+  
